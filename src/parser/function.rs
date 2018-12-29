d10cd022b35f38f7449c77af7ea6204a336f516a
Sam Jackson <sam@clique.app>
2018-12-28 22:31:00 -0500

    test(string.rs): Added a rudimentary test for string parsing

--- src/parser/function.rs
+++ src/parser/function.rs
@@ -1,6 +1,5 @@
 use crate::parser::{ Rule, statement::Statement };
 
-pub fn parse(function: pest::iterators::Pair<'_, Rule>) -> Statement {
-    println!("Parsed function\n{:?}\n", function.as_str());
+pub fn parse(_function: pest::iterators::Pair<'_, Rule>) -> Statement {
     Statement::Function
 }
--- src/parser/string.rs
+++ src/parser/string.rs
@@ -11,3 +11,21 @@ pub fn parse(import: pest::iterators::Pair<'_, Rule>) -> String {
     
     string
 }
+
+#[cfg(test)]
+mod tests {
+
+    use pest::Parser;
+    use crate::parser::{ IronParser, Rule, string };
+
+    #[test]
+    fn string_literal() {
+        let test_literal = "\"Hello world!\"";
+        let test_ast = IronParser::parse(Rule::string_literal, test_literal)
+            .unwrap_or_else(|e| panic!("{}", e));
+
+        for node in test_ast {
+            assert_eq!(string::parse(node), String::from("Hello world!"));
+        }
+    }
+}
